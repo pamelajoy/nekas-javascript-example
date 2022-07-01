1bbe9d0ba94fe50e78d1d896163a1c6cd21a6f07
Pamela Buschbacher <pbuschbacher@gmail.com>
2022-07-01 10:10:53 -0500

    Adds comments for clarity

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const changeBoxColor = (box) => {
+    /** 
+     * https://developer.mozilla.org/en-US/docs/Web/API/Element/className
+     * The name className is used for this property instead of class 
+     * because of conflicts with the "class" keyword in many languages which are used to manipulate the DOM.
+     */
     if (box.className === 'box blueviolet-box'){
         box.className = 'box yellow-box';
     } else {
@@ -16,11 +21,24 @@ const changeTextColor = (textArray) => {
 }
 
 const changeColor = () => {
+    /**
+     * There may only be 1 element with any given ID on a page, so this is very specific and will return 1 element
+     */
     const box = document.getElementById("changeColor");
     changeBoxColor(box);
 
+    /**
+     * Many elements on a page can share a class, this will return all of those elements. 
+     * It will always return an HTMLCollection, even if there is only 1.
+     */
     const textElements = document.getElementsByClassName("changeText");
+    
+    /**
+     * HTMLCollections may look like an array, but they are not. 
+     * We are converting it into an array here so that we can run an array method on it (forEach)
+     */
     const arrayOfTextElements = Array.from(textElements);
+    
     changeTextColor(arrayOfTextElements);
 }
 
